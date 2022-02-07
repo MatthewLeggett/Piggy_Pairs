24211baf2c1fe017e270af18eeba8e235dec703a
MatthewLeggett <matthew.leggett98@gmail.com>
2022-02-07 21:27:54 +0000

    card flip added upon click

---
+++ assets/js/script.js
@@ -0,0 +1,24 @@
+const cards = document.querySelectorAll('.card');
+
+let hasFlippedCard = false;
+let firstCard, secondCard;
+
+function flipCard() {
+  this.classList.add('flip');
+
+  if (!hasFlippedCard) {
+      hasFlippedCard = true;
+      firstCard = this;
+  } else {
+      hasFlippedCard = false;
+      secondCard = this;
+
+      console.log(firstCard.dataset.framework);
+      console.log(secondCard.dataset.framework);
+      
+  } 
+  
+}
+
+
+cards.forEach(card => card.addEventListener('click', flipCard));
